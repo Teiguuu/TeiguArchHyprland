7d5cabd2bed3f271dc79a3b36ab5abd8704effa9
Samost4 <bloodflov@gmail.com>
2026-01-02 21:57:34 +0200

    fix(power): battery limit toggle logic and waybar integration

--- config/hypr/scripts/battery_limit.sh
+++ config/hypr/scripts/battery_limit.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-if [ "$1" == "80" ]; then
-    sudo /usr/bin/ideapad-cm enable ideapad_laptop
-elif [ "$1" == "100" ]; then
+
+# Получаем текущий статус
+STATUS=$(/usr/bin/ideapad-cm status)
+
+if [[ $STATUS == *"enabled"* ]]; then
+    # Если включен лимит (80%), выключаем его (будет 100%)
     sudo /usr/bin/ideapad-cm disable ideapad_laptop
+else
+    # Если выключен, включаем (будет 80%)
+    sudo /usr/bin/ideapad-cm enable ideapad_laptop
 fi
+
+# Обновляем waybar
 pkill -RTMIN+8 waybar
